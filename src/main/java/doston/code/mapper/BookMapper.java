6b8f6b3db47b5b7235582d56e1a547fd589ae92c
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-12-18 19:26:51 +0500

    feat(book): add BookMapper.java to map book

---
+++ src/main/java/doston/code/mapper/BookMapper.java
@@ -0,0 +1,30 @@
+package doston.code.mapper;
+
+import doston.code.dto.request.BookRequestDTO;
+import doston.code.dto.response.BookResponseDTO;
+import doston.code.entity.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BookMapper {
+    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "authorId", target = "authorId")
+    @Mapping(source = "genreId", target = "genreId")
+    @Mapping(source = "count", target = "count")
+    @Mapping(source = "createdDate", target = "createdDate")
+    @Mapping(source = "updatedDate", target = "updatedDate")
+    BookResponseDTO toDto(Book book);
+
+
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "authorId", target = "authorId")
+    @Mapping(source = "genreId", target = "genreId")
+    @Mapping(source = "count", target = "count")
+    Book toEntity(BookRequestDTO requestDTO);
+
+}
