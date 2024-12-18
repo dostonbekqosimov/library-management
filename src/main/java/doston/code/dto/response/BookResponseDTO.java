ba64074d91a3aa9ed0942fbea4df24d47f4d45f7
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-12-18 19:26:32 +0500

    feat(book): add request, response dtos for book

--- src/main/java/doston/code/dto/response/BookResponseDTO.java
+++ src/main/java/doston/code/dto/response/BookResponseDTO.java
@@ -1,4 +1,18 @@
 package doston.code.dto.response;
 
-public record BookResponseDTO() {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record BookResponseDTO(
+        Long id,
+        String title,
+        Long authorId,
+        Long genreId,
+        Integer count,
+        LocalDateTime createdDate,
+        LocalDateTime updatedDate
+) {
 }
