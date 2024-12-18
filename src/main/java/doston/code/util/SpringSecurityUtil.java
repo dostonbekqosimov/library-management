deb5d1f56230d8af072b90cb7ad153a0cbf9aa70
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-12-18 14:05:41 +0500

    feat(librarian): add SpringSecurityUtil.java to get some data about current user

---
+++ src/main/java/doston/code/util/SpringSecurityUtil.java
@@ -0,0 +1,33 @@
+package doston.code.util;
+
+
+import doston.code.enums.ProfileRole;
+import doston.code.security.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SpringSecurityUtil {
+
+
+    public static CustomUserDetails getCurrentEntity() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetail = (CustomUserDetails) authentication.getPrincipal();
+
+        return userDetail;
+    }
+
+    public static Long getCurrentUserId() {
+        CustomUserDetails userDetail = getCurrentEntity();
+
+        return userDetail.getId();
+    }
+
+
+
+    public static ProfileRole getCurrentUserRole(){
+
+        CustomUserDetails userDetail = getCurrentEntity();
+
+        return userDetail.getRole();
+    }
+}
