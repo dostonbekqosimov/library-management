9a58ad2229213992f4baaa16053cf8a740b7c32e
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-12-18 14:04:12 +0500

    feat(exception): add some exceptions

---
+++ src/main/java/doston/code/exception/DataNotFoundException.java
@@ -0,0 +1,8 @@
+package doston.code.exception;
+
+public class DataNotFoundException extends RuntimeException {
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
